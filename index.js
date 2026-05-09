b0c39e2a7821b80a62f65a738f18de20a96f56e2
Rahat Husain khan <123046685+Rahat-coder971@users.noreply.github.com>
2026-05-09 18:22:02 +0530

    Update root route response message

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get("/users", async (req, res) => {
 
 // 🔴 Root route
 app.get("/", (req, res) => {
-  res.send("🚀 App is running and connected to MongoDB!");
+  res.send("🚀 App is running and connected to MongoDB! and is updated");
 });
 
 // 🔹 Start server
